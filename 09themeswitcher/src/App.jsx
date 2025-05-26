ad0ad0472483c6790ea878e693bfd3789aea7042
yogeshhacker <yogeshsingh9644968998@gmail.com>
2025-05-26 13:51:12 +0530

    context

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return;
+  <div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+    {/* Theme Button */}
+      <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+            
+      <div className="w-full max-w-sm mx-auto">
+        {/* Card  */}
+      </div>
+    </div>
+  </div>;
+}
+
+export default App;
